8fa31cda292a7305cc4099482a333a4cd7c66669
Jeongkyo Kim <chavitak@gmail.com>
2021-10-13 21:32:01 +0900

    test(Position): 자동차 위치가 음수인 경우에 대한 테스트를 추가한다
    - 자동차 위치가 음수로 시작하는 경우에 대한 테스트를 추가한다

---
+++ src/test/java/racinggame/domain/PositionTest.java
@@ -0,0 +1,24 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racinggame.exception.NegativePositionException;
+import racinggame.exception.NotIntegerInputException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+
+    @Test
+    @DisplayName("자동차 위치가 음수인 경우 예외를 던진다")
+    void fromTest() {
+        assertThatThrownBy(() -> {
+            // when
+            Position position = Position.from(-1);
+        })
+                // then
+                .isInstanceOf(NegativePositionException.class)
+                .hasMessageContaining("자동차의 위치는 음수가 될 수 없습니다");
+    }
+}
