2f7d64f90ee2a9f6353eea41a2940c74fd9d7232
Jeongkyo Kim <chavitak@gmail.com>
2021-10-13 19:02:53 +0900

    fix(exception): NameLengthExcessException을  IllegalArgumentException을 상속받도록 변경한다
    - NameLengthExcessException을 IllegalArgumentException을 상속받도록
      변경하여 반복시행시 상위에 있는 try-catch 문에 반응하도록 변경한다.

--- src/main/java/racinggame/exception/NameLengthExcessException.java
+++ src/main/java/racinggame/exception/NameLengthExcessException.java
@@ -1,8 +1,6 @@
 package racinggame.exception;
 
-import java.util.NoSuchElementException;
-
-public class NameLengthExcessException extends NoSuchElementException {
+public class NameLengthExcessException extends IllegalArgumentException {
     public NameLengthExcessException() {
         super("자동차 이름은 5자 이하만 입력 가능합니다");
     }
